d5a1e2ae6b96529eb0f57038a82ff89d1b159cbe
Gianfranco <gfman@inwind.it>
2023-05-08 18:18:18 +0200

    improve basic version

--- resources/views/view-singer-data.blade.php
+++ resources/views/view-singer-data.blade.php
@@ -9,18 +9,19 @@
             </a>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                 @foreach ($singers as $singer )
-                    <li> <a href="{{route('showsingersongs', $singer->id)}}">{{$singer->name}}</a></li>
+                    <li> <a class="singer-lst" href="{{route('showsingersongs', $singer->id)}}">{{$singer->name}} -- {{$singer->birthdate}} -- {{$singer->gender}}</a></li>
                 @endforeach
             </ul>
         </div>
 
         @if($displaySongs)
         <div class="wht">
+            @if( count($songs) > 0)
             @for ($i = 0; $i < count($songs); $i ++)
                 <div class="flx">
                     <div class="frm-cnt dv-frm">
                         <ul class="list-group">
-                            <li class="list-group-item">Titlo: <span>{{$songs[$i]->title}}</span></li>
+                            <li class="list-group-item">Titolo: <span>{{$songs[$i]->title}}</span></li>
                         </ul>
                     </div>
                     <div class="frm-cnt dv-frm">
@@ -30,6 +31,13 @@
                     </div>
                 </div>
             @endfor
+            @else
+            <div class="flx">
+                <div class="frm-cnt">
+                    <label class="list-group-item">Nessuna canzone per questo interprete</label>
+                </div>
+            </div>
+            @endif
         </div>
         @endif
     </div>
